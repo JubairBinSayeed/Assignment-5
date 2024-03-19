3d99e80f2cb6b6255132562d5bb287f4f3728718
JubairBinSayeed <jubairbinsayeed@gmail.com>
2024-03-19 16:15:51 +0600

    DONE!!!

--- js/script.js
+++ js/script.js
@@ -6,36 +6,15 @@ for(const btn of allBtn){
         h1.innerHTML = `
         <h1>Economy 550</h1>
         <br>
-        ` 
+        `
+ 
         btn.classList.add('hover:bg-green-500');
         const eventTicket = event.target.innerText+h1.innerText;
-        const price = document.getElementById("ticket-price");
-        document.getElementById("show-bus-number").append(eventTicket)
-        // document.getElementById("Total").append(price);
-        const budget = document.getElementById("budget").innerText;
-        const turnBudgetToNumber = parseInt(budget);
-        const decreaseOnClickBudget = turnBudgetToNumber  - 1;
-        const decreaseOnClickBudgetTwo = turnBudgetToNumber  - 1 - 1;
-        const decreaseOnClickBudgetThree = turnBudgetToNumber  - 1 - 1 - 1;
-        const decreaseOnClickBudgetFour = turnBudgetToNumber  - 1 - 1 - 1 - 1;
-        const decreaseOnClickBudgetFive = turnBudgetToNumber  - 1 - 1 - 1 - 1 - 1;
-        const decreaseOnClickBudgetSix = turnBudgetToNumber  - 1 - 1 - 1 - 1 - 1 - 1;
-        const decreaseOnClickBudgetSeven = turnBudgetToNumber  - 1 - 1 - 1 - 1 - 1 - 1 - 1;
-        const decreaseOnClickBudgetEight = turnBudgetToNumber  - 1 - 1 - 1 - 1 - 1 - 1 - 1 - 1 ;
-        console.log(decreaseOnClickBudget,decreaseOnClickBudgetTwo,decreaseOnClickBudgetThree,decreaseOnClickBudgetFour,decreaseOnClickBudgetFive,decreaseOnClickBudgetSix,decreaseOnClickBudgetSeven,decreaseOnClickBudgetEight);
-        // document.getElementById("budget").classList.add("hide")
-        // document.getElementById("budget").replace(decreaseOnClickBudget);   
-        if(decreaseOnClickBudget === 0){
-            alert("Out of seats please try again tomorrow");
-        } 
+        // const price = document.getElementById("ticket-price");
+        document.getElementById("show-bus-number").append(eventTicket); 
+
     })
 }
-
-// function getUpdatedValue(value){
-//     const totalCost = getConvertedValue("ticket-price")
-//     const sum = totalCost + parseInt(value);
-//     document.getElementById("ticket-price").innerText = sum 
-// }
 function getConvertedValue(id){
     const ticketPrice = document.getElementById(id).innerText
     const parseIntTicketPrice = parseInt(ticketPrice);
